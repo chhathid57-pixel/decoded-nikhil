739d826a76bf0d94463db6d4771f0b2e789d25fc
chhathid57-pixel <chhathid57@gmail.com>
2026-09-06 10:26:41 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2419,7 +2419,6 @@ await runner.setup()
 port = int(os.environ.get("PORT", 8080))
 site = web.TCPSite(runner, "0.0.0.0", port)
 await site.start()
-
     try:
     
         await dp.start_polling(bot)
