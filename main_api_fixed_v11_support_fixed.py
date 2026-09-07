a0a3c00d4848b0cc4784002c7fed85291db53c3a
chhathid57-pixel <chhathid57@gmail.com>
2026-09-07 17:30:46 +0530

    Update main_api_fixed_v11_support_fixed.py

--- main_api_fixed_v11_support_fixed.py
+++ main_api_fixed_v11_support_fixed.py
@@ -442,7 +442,7 @@ def init_db() -> None:
         ('bot_status', 'ON'),
         ('how_to_video', 'None'),
         ('all_files_link', 'None'),
-       ('fampay_api_key', 'FAM_20c2ed43ec69c456bb25d23221854b8f46cfa9b460cebe85'),
+       ('fampay_api_key',fam_1a6bad3a52bb47e02ff7c301f9da3b4e0295ae2f'),
  
         ('fampay_base_url', 'https://fampay.anujbots.xyz/'),
 
