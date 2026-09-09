6e58b148d267d738294852f060de786db3121f6d
chhathid57-pixel <chhathid57@gmail.com>
2026-09-09 19:07:09 +0530

    Update main_api_fixed_v11_support_fixed.py

--- main_api_fixed_v11_support_fixed.py
+++ main_api_fixed_v11_support_fixed.py
@@ -2881,9 +2881,7 @@ async def fetch_external_key(product_id: str, duration: str, android_id: str = "
         clean_product_id = product_id
 
     payload = {
-        "product_id": clean_product_id,
         "variant_id": clean_product_id,
-        "duration": duration,
         "quantity": 1
     }
 
